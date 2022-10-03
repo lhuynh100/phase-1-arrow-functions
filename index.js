7ca003f16ec5489972f90058284a55af24b613b3
Le Huynh <lhuynh100@gmail.com>
2022-10-03 14:15:48 -0500

    commit message

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+let divide = function (a, b) {
+    return a / b;
+} ;
+console.log(divide(2000, 100));
+
+const square = (num1, num2 ) => 2 * 2;
+console.log(square(2,2))
+
+let add = (a, b) => a + b;
+console.log(add(3,4));
